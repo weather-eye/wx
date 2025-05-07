13bd41d0219374577813a8425ae9282ef3be8796
isedwards <ian@myacorn.com>
2025-05-07 09:49:52 +0100

    Initial commit - cookiecutter-pyproject

---
+++ wx/__init__.py
@@ -0,0 +1,7 @@
+"""
+Please refer to the documentation provided in the README.md
+"""
+
+__author__ = """Ian Edwards"""
+__email__ = "ian@myacorn.com"
+__version__ = "0.1.0"
---
+++ wx/wx.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+"""
+wx.py
+
+WeatherEye Controller
+"""
+
+# import os
+
+__author__ = 'Ian Edwards'
+__copyright__ = 'Copyright 2025, Ian Edwards'
+__license__ = 'MIT'
+
+__version__ = '0.1.0'
+__maintainer__ = 'Ian Edwards'
+__email__ = 'ian@myacorn.com'
+__status__ = 'Development'
+
+
+def add(first_term, second_term):
+    """Example trivial function which returns the sum of two numbers.
+
+    Args:
+        first_term (int):  The first parameter.
+        second_term (str): The second parameter.
+
+    Returns:
+        int: The sum of the two parameters.
+    """
+    sum = first_term + second_term
+    return sum
+
+
+def subtract(first_term, second_term):
+    """Example trivial function which returns the difference of two numbers.
+
+    Args:
+        first_term (int):  The first parameter.
+        second_term (str): The second parameter.
+
+    Returns:
+        int: The difference between the two parameters.
+    """
+    difference = first_term - second_term
+    return difference
